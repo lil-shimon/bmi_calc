463ceb6da3703855a9b0707c025d05d21b505188
lil-shimon <shimon101018@gmail.com>
2021-06-27 22:14:41 +0900

    add

--- lib/widget/base_card.dart
+++ lib/widget/base_card.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 /// require colour
+/// optional childCard
 class BaseCard extends StatelessWidget {
 
-  BaseCard({ @required this.colour });
+  BaseCard({ @required this.colour, this.childCard });
 
   final Color colour;
+  final Widget childCard;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: childCard,
       decoration: BoxDecoration(
         color: colour,
         borderRadius: BorderRadius.circular(15.0),
